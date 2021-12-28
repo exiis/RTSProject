29d591e261b6a6040c2a57a145f5bcf3b9ac554c
ExiiS <as_hy@naver.com>
2021-12-28 17:39:23 +0900

    first

---
+++ RTS Project/Assets/Script/init_script.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class init_script : MonoBehaviour
+{
+    public Transform ground;
+    public Transform background;
+    
+    public List<Transform> ground_set;
+    void init_map(){
+        for(int z=0; z<32; z++){
+                    for(int x=0; x<32; x++){
+                        ground_set.Add(Instantiate(ground, new Vector3(x*3.5f, 0, z*3.5f), Quaternion.identity));
+                        //ground_set[cnt] = Instantiate(ground, new Vector3(x*3.5f, 0, z*3.5f), Quaternion.identity);
+                    }
+                }
+        for(int i=0; i<3; i++){
+            for(int j=0; j<4; j++){
+                Instantiate(background, new Vector3(i*50, -1, j*35), Quaternion.identity);
+            }
+        }
+    }
+
+    void init_starting(){
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        init_map();
+        init_starting();
+
+        GameObject[] ground_set = GameObject.FindGameObjectsWithTag("ground");
+        Debug.Log(ground_set.Length);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RTS Project/Assets/Script/unit.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour
+{
+    /* < list info >
+    0 : moveSpeed
+    1 : attackDamage
+    2 : attackSpeed
+    3  defense
+    4 : sight */
+    List<float> stat = new List<float>();
+
+   /* other setting */
+    public Transform selectEffect; 
+    private CharacterController characterController;
+    Vector3 pos, mousePos; //for unit action
+    bool unitSelected = false; //for unit state
+    public GameObject Plane;
+
+    /* <unit state info>
+    1. stop
+    2. move
+    3. attack
+    4. Interaction */
+    int unitState = 0;
+    void init_pos(){
+        mousePos = Input.mousePosition;
+        pos = Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z)); 
+        Debug.Log(pos);
+    }
+
+    /* default setting*/
+    private void Awake(){
+        characterController = GetComponent<CharacterController>();
+        for(int i=0; i<5; i++)
+            stat.Add(0.0f);
+    }
+
+    void getObjPos(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Vector3 worldPosition = new Vector3();
+        MeshCollider meshCollider = Plane.GetComponent<MeshCollider>();
+        if(meshCollider.Raycast(ray, out hit, 1000)){
+            worldPosition = hit.point;
+        }
+        Debug.Log(worldPosition);
+        pos = worldPosition;
+    }
+
+    void Update() {
+        if(Input.GetMouseButton(0)){
+            /*
+            Debug.Log("new!");
+            Instantiate(select, new Vector3(0, 1, 0), select.transform.rotation);
+            */
+            getObjPos();
+            transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * 5);
+        }
+
+        if(Input.GetMouseButton(1)){
+            init_pos();
+            transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * 5);
+        }
+    }
+}
